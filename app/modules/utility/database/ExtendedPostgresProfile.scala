1c50ba9b6198dd5e92a017f2b3b1da7b2adbd557
Domantas Petrauskas <dom.petrauskas@gmail.com>
2019-03-01 16:28:22 +0200

    Initial commit
    Implement slick-pg
    
    Add testing utilities and example integration test, add utilities for front-end, improve package structure
    
    Add e2e test for person form and comment about insertOrUpdate issue with Slick

---
+++ app/modules/utility/database/ExtendedPostgresProfile.scala
@@ -0,0 +1,35 @@
+package modules.utility.database
+
+import com.github.tminglei.slickpg._
+import slick.basic.Capability
+
+trait ExtendedPostgresProfile extends ExPostgresProfile
+  with PgArraySupport
+  with PgDate2Support
+  with PgRangeSupport
+  with PgHStoreSupport
+  with PgPlayJsonSupport
+  with PgSearchSupport
+  with PgNetSupport
+  with PgLTreeSupport {
+
+  def pgjson = "jsonb"
+
+  // disable this if issue https://github.com/slick/slick/pull/1983 is still unfixed
+  // because insertOrUpdate queries for tables with a primary key composed of all columns will fail
+  override protected def computeCapabilities: Set[Capability] =
+    super.computeCapabilities + slick.jdbc.JdbcCapabilities.insertOrUpdate
+
+  override val api = new API with ArrayImplicits
+    with DateTimeImplicits
+    with PlayJsonImplicits
+    with NetImplicits
+    with LTreeImplicits
+    with RangeImplicits
+    with HStoreImplicits
+    with SearchImplicits
+    with SearchAssistants {
+  }
+}
+
+object ExtendedPostgresProfile extends ExtendedPostgresProfile
---
+++ build.sbt
@@ -0,0 +1,30 @@
+name := """play-scala-opinionated-backend-seed"""
+
+version := "2.7.x"
+
+lazy val root = (project in file(".")).enablePlugins(PlayScala)
+
+scalaVersion := "2.12.8"
+
+libraryDependencies += guice
+
+libraryDependencies += "org.postgresql" % "postgresql" % "42.2.5"
+
+libraryDependencies += "com.typesafe.play" %% "play-slick" % "4.0.0"
+libraryDependencies += "com.typesafe.play" %% "play-slick-evolutions" % "4.0.0"
+
+libraryDependencies += "com.github.tminglei" %% "slick-pg" % "0.17.2"
+libraryDependencies += "com.github.tminglei" %% "slick-pg_play-json" % "0.17.2"
+
+libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "4.0.1" % Test
+
+libraryDependencies += "com.dimafeng" %% "testcontainers-scala" % "0.22.0" % Test
+libraryDependencies += "org.testcontainers" % "postgresql" % "1.10.1" % Test
+
+libraryDependencies += specs2 % Test
+
+scalacOptions ++= Seq(
+  "-feature",
+  "-deprecation",
+  "-Xfatal-warnings"
+)
