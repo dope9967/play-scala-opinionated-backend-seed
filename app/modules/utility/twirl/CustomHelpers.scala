f03ea4f01dfaa005bbfbafb1b2c2aa05aba079cd
Domantas Petrauskas <dom.petrauskas@gmail.com>
2020-02-09 15:00:26 +0200

    Remove obsolete code

--- app/modules/utility/twirl/CustomHelpers.scala
+++ app/modules/utility/twirl/CustomHelpers.scala
@@ -1,11 +1,6 @@
 package modules.utility.twirl
 
-import views.html
-
 object CustomHelpers {
-
-  import views.html.helper.FieldConstructor
-  implicit val myFields = FieldConstructor(html.component.customFieldConstructorTemplate.f)
 
   val AppName = "play-scala-opinionated-backend-seed"
 }
