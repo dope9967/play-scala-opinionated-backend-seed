d2de16a7b74f39e48f98e46dad9b98717e6f9208
Domantas Petrauskas <dom.petrauskas@gmail.com>
2020-02-27 16:25:08 +0200

    Add spec for evolutions, comment out outdated specs

--- test/modules/person/PersonRepositorySpec.scala
+++ test/modules/person/PersonRepositorySpec.scala
@@ -1,6 +1,6 @@
 package modules.person
 
-import org.scalatest.TestData
+import org.scalatest.{Ignore, TestData}
 import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneAppPerTest
@@ -10,6 +10,7 @@ import utility.database.PlayPostgreSQLTest
 
 import scala.concurrent.ExecutionContext
 
+@Ignore
 class PersonRepositorySpec
     extends PlaySpec
     with GuiceOneAppPerTest
@@ -21,27 +22,27 @@ class PersonRepositorySpec
     basicDatabaseTestApplication(container, evolutionsEnabled = false)
   }
 
-  "PersonRepository" must {
-    "create new person John" in {
-      implicit lazy val executionContext = app.injector.instanceOf[ExecutionContext]
-      withEvolutions { () =>
-        val repo   = app.injector.instanceOf[PersonRepository]
-        val person = repo.create("John", 24).futureValue
-
-        person mustEqual Person(1, "John", 24)
-      }
-    }
-
-    "create new person Alex and update age" in {
-      implicit lazy val executionContext = app.injector.instanceOf[ExecutionContext]
-      withEvolutions { () =>
-        val repo = app.injector.instanceOf[PersonRepository]
-
-        val newPerson     = repo.create("Alex", 33).futureValue
-        val updatedPerson = repo.save(newPerson.copy(age = 34)).futureValue
-
-        updatedPerson mustEqual Person(1, "Alex", 34)
-      }
-    }
-  }
+//  "PersonRepository" must {
+//    "create new person John" in {
+//      implicit lazy val executionContext = app.injector.instanceOf[ExecutionContext]
+//      withEvolutions { () =>
+//        val repo   = app.injector.instanceOf[PersonRepository]
+//        val person = repo.create("John", 24).futureValue
+//
+//        person mustEqual Person(1, "John", 24)
+//      }
+//    }
+//
+//    "create new person Alex and update age" in {
+//      implicit lazy val executionContext = app.injector.instanceOf[ExecutionContext]
+//      withEvolutions { () =>
+//        val repo = app.injector.instanceOf[PersonRepository]
+//
+//        val newPerson     = repo.create("Alex", 33).futureValue
+//        val updatedPerson = repo.save(newPerson.copy(age = 34)).futureValue
+//
+//        updatedPerson mustEqual Person(1, "Alex", 34)
+//      }
+//    }
+//  }
 }
