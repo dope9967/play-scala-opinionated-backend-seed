990b32d4821a33908034cead7c574fac97ad43a4
Domantas Petrauskas <dom.petrauskas@gmail.com>
2020-02-27 17:12:15 +0200

    Change Play Postgres test harness to require providing database as argument

--- test/utility/database/PlayPostgreSQLTest.scala
+++ test/utility/database/PlayPostgreSQLTest.scala
@@ -2,62 +2,29 @@ package utility.database
 
 import com.dimafeng.testcontainers.{ForEachTestContainer, PostgreSQLContainer}
 import org.scalatest.Suite
-import org.scalatest.compatible.Assertion
 import org.scalatestplus.play.AppProvider
-import play.api.db.DBApi
+import play.api.db.Database
 import play.api.db.evolutions.Evolutions
-
-import scala.concurrent.{ExecutionContext, Future}
-import scala.util.control.NonFatal
 
 trait PlayPostgreSQLTest extends Suite with AppProvider with ForEachTestContainer {
 
   override final val container: PostgreSQLContainer = PostgreSQLContainer("postgres:12.1-alpine")
 
-  def applyEvolutions(): Unit = {
-    lazy val databaseApi = app.injector.instanceOf[DBApi]
-    val database         = databaseApi.database("default")
-    try {
-      Evolutions.applyEvolutions(database)
-      database.shutdown()
-    } catch {
-      case NonFatal(e) =>
-        database.shutdown()
-        throw e
-    }
+  def applyEvolutions(database: Database): Unit = {
+    Evolutions.applyEvolutions(database)
   }
 
-  def unapplyEvolutions(): Unit = {
-    lazy val databaseApi = app.injector.instanceOf[DBApi]
-    val database         = databaseApi.database("default")
-    try {
-      Evolutions.cleanupEvolutions(database)
-      database.shutdown()
-    } catch {
-      case NonFatal(e) =>
-        database.shutdown()
-        throw e
-    }
+  def unapplyEvolutions(database: Database): Unit = {
+    Evolutions.cleanupEvolutions(database)
   }
 
-  def withEvolutions(
-      assertionFun: () => Assertion
-  ): Assertion = {
-    applyEvolutions()
-    val assertion = assertionFun.apply()
-    unapplyEvolutions()
-    assertion
+  def withEvolutions(database: Database)(
+      block: () => Unit
+  ): Unit = {
+    applyEvolutions(database)
+    block()
+    unapplyEvolutions(database)
   }
 
-  def withEvolutions(
-      futureAssertionFun: () => Future[Assertion]
-  )(implicit ec: ExecutionContext): Future[Assertion] = {
-    applyEvolutions()
-    futureAssertionFun
-      .apply()
-      .map { assertion =>
-        unapplyEvolutions()
-        assertion
-      }
-  }
+  //TODO consider requiring test database configuration so DBApi.database returns an already initialized instance
 }
